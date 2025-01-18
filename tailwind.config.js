39c13ecbd507cbeacd6bc97c68bf547168e76ecf
Andrew <bakarekeandrew@gmail.com>
2025-01-18 20:13:51 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()] ,
 }
+
+
 
